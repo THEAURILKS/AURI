b2dc6ad81fe3394ff972b1d478a00b0d5fce313a
THEAURILKS <147957840+THEAURILKS@users.noreply.github.com>
2023-10-28 11:52:23 +0800

    Update AURI.py

--- AURI.py
+++ AURI.py
@@ -1312,7 +1312,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrahsan-cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("https://raw.githubusercontent.com/THEAURILKS/AURI/main/approval.txt").text
+	r1=requests.get("https://github.com/THEAURILKS/AURI/blob/main/approval.txt").text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
